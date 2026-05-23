692b0202effdef2b978e97ab4497619e9cc122c9
aeriaIz <su.yildiz@msn.com>
2026-05-23 15:59:07 +0300

    chore: remove stale test and utility scripts

--- test_db.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-import json
-
-conn = psycopg2.connect("postgresql://user:password@localhost:5432/evidence_db")
-cur = conn.cursor()
-cur.execute("SELECT payload_json FROM outbox_events ORDER BY created_at DESC LIMIT 1;")
-payload = cur.fetchone()[0]
-print(json.dumps(payload, indent=2))
